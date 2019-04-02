034630e935c5af46cf08feb1a0f331325718ba78
GBTC440017UR <craighunt210@gmail.com>
2019-04-01 20:17:42 -0400

    Initial commit

---
+++ src/main/java/com/cwhcode/springboot17/Springboot17Application.java
@@ -0,0 +1,13 @@
+package com.cwhcode.springboot17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot17Application.class, args);
+	}
+
+}
